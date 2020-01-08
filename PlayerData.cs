7c36642d562a87ea6c933e56e9933c21e05554bd
Mariusz Przybysz <Devmariusz.h@gmail.com>
2020-01-08 17:06:26 -0600

    Connect to desktop clone folder

---
+++ PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+//Create a scriptable object.
+
+[CreateAssetMenu(fileName = "Player", menuName = "Player/Player Data", order = 51)]
+public class PlayerData : ScriptableObject
+{
+    [SerializeField]
+    private Sprite playerIcon = null;
+    public Sprite PlayerIcon { get { return playerIcon; } }
+
+    [SerializeField]
+    private string playerName = "none";
+    public string PlayerName { get { return playerName; } }
+
+    [SerializeField]
+    private float playerHealth = 40;
+    public float PlayerHealth { get { return playerHealth; } }
+}
